74c524050c3723bfd617caa584d2aaa2a50decc9
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-03-31 00:38:17 -0400

    Added max width to the footer

--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -7,7 +7,7 @@ import { FaFacebookF, FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-2 absolute bottom-0 w-full">
-      <div className=" px-4 flex flex-col items-center w-full">
+      <div className=" flex flex-col mx-auto items-center w-full max-w-7xl">
         {/* Logo & Socials (Same Row) */}
         <div className="flex flex-col md:flex-row md:px-10 items-center justify-between w-full">
           {/* Logo */}
