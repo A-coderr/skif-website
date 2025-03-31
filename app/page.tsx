2d6078d16940306b06da44be8e67071021e15eab
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-03-31 00:25:47 -0400

    Created a footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import NavMenu from "./components/NavMenu";
+import Footer from "./sections/Footer";
 
 export default function Home() {
   return (
     <>
       <NavMenu />
+      <Footer />
     </>
   );
 }
