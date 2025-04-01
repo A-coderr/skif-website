234bcc0e312d6399fbb9df577d6762697c6991f1
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-03-31 21:58:37 -0400

    Made it responsive.

--- app/sections/Karate.tsx
+++ app/sections/Karate.tsx
@@ -9,7 +9,7 @@ export const Karate = () => {
   return (
     <>
       <section className="max-w-7xl mx-auto min-h-[70vh] flex items-center justify-center">
-        <div className="flex flex-col md:flex-row items-center md:items-start gap-12">
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-12 p-6">
           {/* Left Side: Text */}
           <div className="w-full md:w-1/2 text-left md:text-left space-y-6">
             <h1 className="text-5xl font-extrabold text-left text-gray-800 mb-12">
@@ -17,13 +17,13 @@ export const Karate = () => {
             </h1>
             <p className="text-md leading-relaxed text-gray-800 transition-all duration-500 hover:text-amber-700">
               The word <strong>karate</strong> is a combination of two Japanese
-              characters: <strong>kara</strong>, meaning "empty," and{" "}
-              <strong>te</strong>, meaning "hand"; thus karate means "empty
-              hand." Adding the suffix <strong>-do</strong>, meaning "way" (
-              <strong>karate-do</strong>), implies karate as a total way of life
-              that goes well beyond self-defense applications. In traditional
-              karate-do, we always keep in mind that the true opponent is
-              oneself.
+              characters: <strong>kara</strong>, meaning &quot;empty,&quot; and{" "}
+              <strong>te</strong>, meaning &quot;hand&quot;; thus karate means
+              &quot;empty hand.&quot; Adding the suffix <strong>-do</strong>,
+              meaning &quot;way&quot; (<strong>karate-do</strong>), implies
+              karate as a total way of life that goes well beyond self-defense
+              applications. In traditional karate-do, we always keep in mind
+              that the true opponent is oneself.
             </p>
             <p className="text-md leading-relaxed text-gray-800 transition-all duration-500 hover:text-amber-700">
               Karate is many things. It is an excellent and well-balanced form
@@ -45,7 +45,7 @@ export const Karate = () => {
           </div>
 
           {/* Right Side: Image */}
-          <div className="w-full md:w-1/2">
+          <div className="w-full md:w-1/2 flex justify-center items-center">
             <div className="relative overflow-hidden shadow-lg transition-all duration-500">
               <Image
                 src="/hero.jpg"
