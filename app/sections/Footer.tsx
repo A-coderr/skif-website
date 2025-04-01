556a11686cf7caad036699b39d02f8befba5c21f
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-04-01 11:07:22 -0400

    Added animations

--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -2,19 +2,33 @@
 
 import Link from "next/link";
 import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const fadeInUp = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
+};
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white pt-6 bottom-0 w-full">
-      <div className="flex flex-col md:flex-row md:justify-between mx-auto w-full max-w-7xl px-6 py-6 text-center md:text-left">
+    <motion.footer
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      className="bg-gray-900 text-white pt-6 bottom-0 w-full"
+    >
+      <motion.div
+        variants={fadeInUp}
+        className="flex flex-col md:flex-row md:justify-between mx-auto w-full max-w-7xl px-6 py-6 text-center md:text-left"
+      >
         {/* Location */}
-        <div className="mt-6 md:mt-0">
+        <motion.div variants={fadeInUp} className="mt-6 md:mt-0">
           <h3 className="text-lg font-semibold mb-2 text-gray-500">Location</h3>
           <p className="text-sm">2233 Hurontario St, Mississauga, ON L5A 2E9</p>
-        </div>
+        </motion.div>
 
         {/* Working Hours */}
-        <div className="mt-6 md:mt-0">
+        <motion.div variants={fadeInUp} className="mt-6 md:mt-0">
           <h3 className="text-lg font-semibold mb-2 text-gray-500">
             Working Hours
           </h3>
@@ -27,10 +41,10 @@ const Footer = () => {
             <p className="text-sm">Sunday: Closed</p>
             <p className="text-sm">Monday: Closed</p>
           </div>
-        </div>
+        </motion.div>
 
         {/* Social Media Links */}
-        <div className="mt-4 md:mt-0">
+        <motion.div variants={fadeInUp} className="mt-4 md:mt-0">
           <h3 className="text-lg font-semibold mb-2 text-gray-500">
             Follow Us
           </h3>
@@ -48,16 +62,16 @@ const Footer = () => {
               <FaInstagram className="w-6 h-6 hover:text-pink-500 transition" />
             </Link>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
 
-      {/* Copyright (Centered Below, Different Color) */}
-      <div className="bg-[#20293b] py-6">
+      {/* Copyright */}
+      <motion.div variants={fadeInUp} className="bg-[#20293b] py-6">
         <p className="text-sm text-gray-500 text-center">
           © {new Date().getFullYear()} SKIF Karate Canada. All rights reserved.
         </p>
-      </div>
-    </footer>
+      </motion.div>
+    </motion.footer>
   );
 };
 
