27ebd36cd6cc3a759afe3b75e8268af4a3474884
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-04-02 22:12:37 -0400

    Added the Schedule page. Ensured that the router is working.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
-import NavMenu from "./components/NavMenu";
-import Footer from "./sections/Footer";
 import HeroSection from "./sections/Hero";
 import Karate from "./sections/Karate";
 
 export default function Home() {
   return (
     <>
-      <NavMenu />
       <HeroSection />
       <Karate />
       {/* Add more sections here as needed */}
-      <Footer />
     </>
   );
 }
