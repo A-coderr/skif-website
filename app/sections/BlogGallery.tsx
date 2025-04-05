6772fdb53e2127f6246071909bb3efaa61907631
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-04-04 23:13:36 -0400

    Added animations

--- app/sections/BlogGallery.tsx
+++ app/sections/BlogGallery.tsx
@@ -57,7 +57,7 @@ const blogPosts = [
 
 export default function BlogGallery() {
   return (
-    <section className="max-w-7xl mx-auto px-4 py-16">
+    <section className="max-w-7xl mx-auto px-4 py-6">
       <motion.h1
         className="mb-6 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-gray-800"
         initial={{ opacity: 0, y: -20 }} //Initial position and opacity
@@ -67,10 +67,13 @@ export default function BlogGallery() {
         Latest Blog Posts
       </motion.h1>
       <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {blogPosts.map((post) => (
-          <div
+        {blogPosts.map((post, index) => (
+          <motion.div
             key={post.id}
             className="bg-white shadow-md overflow-hidden transition-transform hover:scale-102"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.1 }}
           >
             <div className="relative w-full h-48">
               <Image
@@ -96,7 +99,7 @@ export default function BlogGallery() {
                 Read more →
               </Link>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </section>
